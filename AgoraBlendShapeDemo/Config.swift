8d0418473dcc54dcd9d05ea10489bdf6cfe9c87b
Shaun Hubbard <shaunhubbard2013@icloud.com>
2023-03-07 16:07:36 -0500

    moved app id to Config file safely, :(

--- AgoraBlendShapeDemo/Config.swift
+++ AgoraBlendShapeDemo/Config.swift
@@ -1,5 +1,5 @@
 //
-//  BlendShapeViewControlle+FileSaving.swift
+//  Config.swift
 //  AgoraBlendShapeDemo
 //
 //  Created by shaun on 3/7/23.
@@ -7,3 +7,4 @@
 
 import Foundation
 
+let appId = "<Your App ID>"
